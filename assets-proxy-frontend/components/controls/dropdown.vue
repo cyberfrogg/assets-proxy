20e030b2a939c851df9598eef87706cef3f7204d
cyberfrogg <cyberfrogg.q@gmail.com>
2024-01-03 20:15:35 +0400

    dropdown

---
+++ assets-proxy-frontend/components/controls/dropdown.vue
@@ -0,0 +1,125 @@
+<template>
+  <div
+      :class="[$style.dropdown, { [$style.dropdownActive]: isActive }]"
+      ref="dropdown"
+      @click="handleDropdownClick"
+  >
+    <div :class="$style.selectedItem">
+      {{ currentItem }}
+    </div>
+    <ul :class="$style.list">
+      <li
+          :class="$style.item"
+          v-for="(item, index) in items"
+          :key="index"
+          @click="handleItemClick(item)"
+      >
+        {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dropdown",
+  data: () => {
+    return {
+      isActive: false,
+      currentItem: ''
+    }
+  },
+  props: {
+    items: {
+      type: Array,
+      default: () => ['sample 1', 'sample 2', 'sample 3', 'sample 4'],
+    },
+  },
+  mounted () {
+    this.currentItem = this.items[0];
+
+    document.addEventListener('click', this.handleClickOutside);
+  },
+  methods: {
+    handleDropdownClick () {
+      if (!this.isActive) {
+        this.isActive = true;
+      }
+    },
+    handleItemClick (item) {
+      this.currentItem = item;
+      this.$emit('select', item)
+      setTimeout(() => { this.isActive = false }, 100);
+    },
+    handleClickOutside(event) {
+      const container = this.$refs.dropdown;
+      if (container && !container.contains(event.target)) {
+        this.isActive = false;
+      }
+    }
+  },
+  beforeDestroy() {
+    document.removeEventListener('click', this.handleClickOutside);
+  },
+};
+</script>
+
+<style module lang="scss">
+  .dropdown {
+    width: 100%;
+    height: 40px;
+    background: var(--ap-input-background-color);
+    cursor: pointer;
+    border-radius: var(--ap-controls-radius);
+    border: 2px solid var(--ap-input-background-color);
+    box-sizing: border-box;
+    position: relative;
+    transition: 0.2s;
+  }
+
+  .selectedItem {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    pointer-events: none;
+    user-select: none;
+  }
+
+  .list {
+    width: calc(100% + 4px);
+    position: absolute;
+    left: -2px;
+    top: calc(100% + 10px);
+    list-style: none;
+    background: var(--ap-input-background-color);
+    border-radius: 10px;
+    filter: drop-shadow(0 0 3px rgba(0, 0, 0, 0.1));
+    overflow: hidden;
+    opacity: 0;
+    pointer-events: none;
+  }
+
+  .item {
+    text-align: center;
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .item:hover{
+    background: var(--ap-input-background-hover-color);
+  }
+
+  .dropdownActive {
+    border-color: var(--ap-gradient-main-color);
+    transition: 0.2s;
+  }
+
+  .dropdownActive .list {
+    opacity: 1;
+    pointer-events: all;
+  }
+</style>
--- assets-proxy-frontend/pages/index.vue
+++ assets-proxy-frontend/pages/index.vue
@@ -14,15 +14,33 @@
           />
         </div>
       </div>
+      <section :class="$style.appArea">
+        <form :class="$style.requestForm">
+          <input
+              :class="$style.urlInputField"
+              placeholder="Insert website url where to video download from..."
+          />
+          <Dropdown
+            :items="['1 hour', '1 day', '1 week', '1 month']"
+            @select="handleLifetimeSelect"
+          />
+        </form>
+      </section>
     </div>
   </div>
 </template>
 
 <script>
   import InlineTextScrollContainer from '~/components/promo/inlineTextScrollContainer.vue';
+  import Dropdown from "~/components/controls/dropdown.vue";
 
   export default {
-    components: {InlineTextScrollContainer},
+    components: {Dropdown, InlineTextScrollContainer},
+    methods: {
+      handleLifetimeSelect (item) {
+        console.log(item);
+      }
+    }
   };
 </script>
 
@@ -70,6 +88,40 @@
     font-style: normal;
     font-weight: 700;
     line-height: normal;
+  }
+
+  .appArea {
+    width: 100%;
+    max-width: 900px;
+    margin-top: 100px;
+  }
+
+  .requestForm {
+    width: 100%;
+    height: 40px;
+    display: grid;
+    grid-template-columns: 1fr 110px 130px;
+    grid-gap: 10px;
+  }
+
+  .urlInputField {
+    width: 100%;
+    padding: 0 12px;
+    height: 40px;
+    background: var(--ap-input-background-color);
+    border: 2px solid var(--ap-input-background-color);
+    box-sizing: border-box;
+    border-radius: var(--ap-controls-radius);
+    transition: 0.2s;
+  }
+
+  .urlInputField:focus{
+    border-color: var(--ap-gradient-main-color);
+    transition: 0.2s;
+  }
+
+  .urlInputField::placeholder {
+    color: var(--ap-input-placeholder-color)
   }
 
 </style>
