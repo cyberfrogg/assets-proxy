5b0f075e25360ec3d3fb4c27ca40abcc4c54b284
cyberfrogg <cyberfrogg.q@gmail.com>
2024-01-04 15:46:23 +0400

    error fix

--- assets-proxy-backend/src/downloader/downloader.service.ts
+++ assets-proxy-backend/src/downloader/downloader.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Video } from '../video/video.entity';
 import { Repository } from 'typeorm';
