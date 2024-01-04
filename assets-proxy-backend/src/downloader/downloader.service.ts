d95f10e36a010d18899593eea3c73d08869c6581
cyberfrogg <cyberfrogg.q@gmail.com>
2024-01-04 16:08:54 +0400

    tiktok dl to buffer

--- assets-proxy-backend/src/downloader/downloader.service.ts
+++ assets-proxy-backend/src/downloader/downloader.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Video } from '../video/video.entity';
 import { Repository } from 'typeorm';
+import { TiktokDL } from '@lucas_monroe/tiktok-api-dl';
+import axios from 'axios';
 
 @Injectable()
 export class DownloaderService {
@@ -25,7 +27,70 @@ export class DownloaderService {
     const mediaType = this.defineMediaType(video.url);
     console.log(mediaType);
 
+    switch (mediaType) {
+      case 'youtube':
+        throw new BadRequestException('youtube not supported');
+      case 'tiktok':
+        await this.downloadTiktokVideo(video);
+        break;
+      default:
+        throw new BadRequestException('external not supported');
+    }
+
     this.isWorkerWorking = false;
+  }
+
+  async downloadTiktokVideo(video: Video) {
+    try {
+      const result = await TiktokDL(video.url);
+      if (result.status !== 'success') {
+        console.error(
+          `DL Failed to download tiktok video ${video.id}`,
+          result.message,
+        );
+        await this.markVideoAsFailed(video.id);
+        return;
+      }
+
+      for (let i = 0; i < result.result.video.length; i++) {
+        try {
+          const videoUrl = result.result.video[0];
+          const videoBuffer = await this.downloadVideoToBuffer(videoUrl);
+          break;
+        } catch (ed) {}
+      }
+
+      throw new Error(
+        `Failed to download results from ${result.result.video.length} tiktok cdn mirrors`,
+      );
+    } catch (e) {
+      console.error(`Failed to download tiktok video ${video.id}`, e);
+      await this.markVideoAsFailed(video.id);
+    }
+  }
+
+  async markVideoAsFailed(videoId: string) {
+    await this.videoRepository.update(
+      { id: videoId },
+      { taskStatus: 'failed' },
+    );
+  }
+
+  async downloadVideoToBuffer(videoUrl: string): Promise<Buffer> {
+    try {
+      const response = await axios.get(videoUrl, {
+        responseType: 'arraybuffer',
+      });
+
+      if (response && response.data) {
+        const buffer: Buffer = Buffer.from(response.data, 'binary');
+        return buffer;
+      } else {
+        throw new Error('Empty response or data');
+      }
+    } catch (error) {
+      throw new Error(`Error downloading video: ${error.message}`);
+    }
   }
 
   defineMediaType(urlString: string) {
